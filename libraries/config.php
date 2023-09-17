73f83dd974be949abe209f77b33ccb807c62e5b4
Tavee Villamar <hello@tavee.ca>
2023-09-16 21:32:01 -0500

    update config

--- libraries/config.php
+++ libraries/config.php
@@ -1,7 +1,7 @@
 <?php
 $site_name = "Tavee's Portfolio";
 
-$site_url = "http://localhost/Taveevil.github.io/";
+$site_url = "http://tavee.ca/";
 $template_url = $site_url . "template/";
 $gallery_url = $template_url . "img/gallery/";
 $projects_url = $template_url . "img/projects/";
